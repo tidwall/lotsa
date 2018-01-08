2b6b40e44a956571d08426fc47e11c0c365d9178
Josh Baker <joshbaker77@gmail.com>
2018-01-07 23:05:53 -0700

    Removed output

--- lotsa_test.go
+++ lotsa_test.go
@@ -1,8 +1,6 @@
 package lotsa
 
 import (
-	"bytes"
-	"strings"
 	"sync/atomic"
 	"testing"
 )
@@ -11,14 +9,10 @@ func TestOps(t *testing.T) {
 	var threads = 4
 	var N = threads * 10000
 	var total int64
-	var wr bytes.Buffer
-	Ops(N, threads, &wr, func(i, thread int) {
+	Ops(N, threads, func(i, thread int) {
 		atomic.AddInt64(&total, 1)
 	})
 	if total != int64(N) {
 		t.Fatal("invalid total")
-	}
-	if !strings.Contains(wr.String(), commaize(N)) {
-		t.Fatal("bad writer")
 	}
 }
